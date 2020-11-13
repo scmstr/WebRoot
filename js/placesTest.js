0240ce512c0ee0a585dfcba956aa45aef19bee33
SCmSTR <scmstr@hotmail.com>
2020-11-13 12:52:41 -0800

    v5 test google places api

--- js/placesTest.js
+++ js/placesTest.js
@@ -20,18 +20,34 @@
 
 
 
-document.getElementById("submitIt").addEventListener("click", function () {        
-    var request = new XMLHttpRequest();
-    //var data;
-    var input = document.getElementById("search").textContent;
+   
+var request = new XMLHttpRequest();
+//var data;
+var input = document.getElementById("search").textContent;
 
-    request.open('GET', "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=" + input + "&key=AIzaSyB6TVzwO2st38aW8DznH6E9NCMIuawc-Bs");
-    request.onload = function() {
-        var response = request.response;
-        var parsedData = JSON.parse(response);
-        console.log(parsedData);
-        //data = parsedData;
-    }
+request.open('GET', "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=" + input + "&key=AIzaSyB6TVzwO2st38aW8DznH6E9NCMIuawc-Bs");
+request.onload = function() {
+    var response = request.response;
+    var parsedData = JSON.parse(response);
+    console.log(parsedData);
+    //data = parsedData;
+}
+request.send();
 
-    request.send();
-});
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET', "https://api.giphy.com/v1/gifs/random?api_key=TQESFhwanOQoqCjuJqBvsrENlKfrh9EV&tag=bike");
+// request.onload = function() {
+//     var response = request.response;
+//     var parsedData = JSON.parse(response);
+//     console.log(parsedData);
+//     //data = parsedData;
+
+//     var outputHere = document.getElementById('outputHere');
+//     //put stuff in the output place on the html
+// }
+
+// request.send();
