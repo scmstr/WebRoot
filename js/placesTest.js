8f399ee8abfc349c860615dfcc8ebfc11e590e29
SCmSTR <scmstr@hotmail.com>
2020-11-13 12:55:53 -0800

    v6 test google places api

--- js/placesTest.js
+++ js/placesTest.js
@@ -23,9 +23,7 @@
    
 var request = new XMLHttpRequest();
 //var data;
-var input = document.getElementById("search").textContent;
-
-request.open('GET', "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=" + input + "&key=AIzaSyB6TVzwO2st38aW8DznH6E9NCMIuawc-Bs");
+request.open('GET', "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=" + "julios" + "&key=AIzaSyB6TVzwO2st38aW8DznH6E9NCMIuawc-Bs");
 request.onload = function() {
     var response = request.response;
     var parsedData = JSON.parse(response);
